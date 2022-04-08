4ee8595dd93858a5a0c102d82ea9c9621b581e49
Willian Stephano <will.stephano@gmail.com>
2022-04-08 20:37:31 -0300

    Criação de comentarios

--- js/main.js
+++ js/main.js
@@ -1,46 +1,44 @@
-//Array com as imagens
+//Objeto com as imagens
 const colecaoImg = [
    {
       id: 1,
-      img: 'Conteudo/slide1.png'
+      img: "Conteudo/slide1.png",
    },
-
+   
    {
       id: 2,
-      img: 'Conteudo/slide2.png'
+      img: "Conteudo/slide2.png",
    },
-
+   
    {
       id: 3,
-      img: 'Conteudo/slide3.png'
-   }
-]
+      img: "Conteudo/slide3.png",
+   },
+];
 
 //Selecionando os elemenos no HTML
-const img = document.querySelector('.slide_img')
+const img = document.querySelector(".slide_img");
 
+let imgAtual = 0;
 
-let imgAtual = 0
-
-//Ao cerregar a pagina chame a funcao 
-window.addEventListener('DOMContentLoaded',function () {
-   mostrarImg()
-})
+//Ao cerregar a pagina chame a funcao
+window.addEventListener("DOMContentLoaded", function () {
+   mostrarImg();
+});
 
 //
 function mostrarImg() {
-   let item = colecaoImg[imgAtual]
-   img.src = item.img
+   let item = colecaoImg[imgAtual];
+   img.src = item.img;
 }
 
 function proximaImg() {
-   imgAtual++
+   imgAtual++;
    if (imgAtual > colecaoImg.length) {
-      imgAtual = 0
-      
+      imgAtual = 0;
    }
-   mostrarImg()
+   mostrarImg();
 }
 
 //
-setInterval(proximaImg,1500)
+setInterval(proximaImg, 1500);
